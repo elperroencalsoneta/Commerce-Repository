29827192577f35c65f97e73583e9157f7edee17f
Luis Alberto <64315789+elperroencalsoneta@users.noreply.github.com>
2023-02-10 17:17:54 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = '0qzf*j&a(lpyjjg8ef22#n4(w936m89cfd%&dz=mf=nhx!l1je'
+
 
 DEBUG = True
 
